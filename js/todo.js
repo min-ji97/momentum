69635bff4cf1799630aa3fb64b5f369e561360e0
jominji <whalswl576@gmail.com>
2024-07-07 23:09:36 +0900

    CSS+js : todo-list 수정/ checkbox추가

--- js/todo.js
+++ js/todo.js
@@ -12,23 +12,81 @@ function saveList(value){
     const li = document.createElement("li");
     const span = document.createElement("span");
     const button = document.createElement("button");
+    const input = document.createElement("input");
 
+
+    input.setAttribute("type","checkbox");
+    input.setAttribute("onchange","changeChecked(this)");
+    input.setAttribute("class",value.id);
+
+    input.checked = value.checked;
     span.innerText = value.text;
-    button.innerText = '❌';
+    
+    // button.innerText = '❌';
+
+    li.appendChild(input);
     li.appendChild(span);
     li.appendChild(button);
     todoList.appendChild(li);
     button.addEventListener('click',deleteList);
+    span.addEventListener('click',pointText);
     li.setAttribute('id', value.id);
-    // li.setAttribute();
+
+    if(input.checked === true){
+        span.classList.add('checked');
+    }else{
+        span.classList.remove('checked');
+    }
     
 }
 
 
-// <li>
-// <span><button>
-//</li><>
-//
+function isChecked(id, bool){
+    function change(lists){
+        if( lists.id === id){
+            return lists.checked = bool;
+        }
+    }
+    Lists.map(change);
+
+    localStorage.setItem(TODOLIST,JSON.stringify(Lists));
+}
+
+function changeChecked(value) {
+
+    const id = Number(value.className);
+    let bool = null;
+    const nextSpanTag = value.nextElementSibling;
+
+    if(value.checked === true) {
+        bool = true;
+        isChecked(id,bool);
+        nextSpanTag.classList.add('checked');
+        if(nextSpanTag.classList.contains('strongText')){
+            nextSpanTag.classList.remove('strongText');
+        }
+
+    }else{
+        bool = false;
+        isChecked(id,bool);
+        nextSpanTag.classList.remove('checked');
+    }
+}
+
+ 
+function pointText(e) {
+
+    console.log('강조 버튼을 클릭하였습니다 ', e.target);
+    const tag = e.target;
+    if(!tag.classList.contains('checked') ){
+        if(tag.classList.contains('strongText')){
+            tag.classList.remove('strongText');
+        }else{
+            tag.classList.add('strongText');
+        }
+    }
+
+}
 
 
 function deleteList(e){
@@ -53,9 +111,11 @@ function InputSubmit(e){
     todoInputValue = todoInput.value;
     todoInput.value=""; 
     // Lists.push(todoInputValue);
+
     const todoObj = {
         text: todoInputValue,
-        id : Date.now()
+        id : Date.now(),
+        checked : false
     }
 
     Lists.push(todoObj);
